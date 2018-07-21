f5373ecee668d36d7e023f5a4dae5efd605f123e
Kepler <hanif.hg@gmail.com>
2018-07-21 14:53:45 +0700

    Add new func

--- stringhelper/helper.go
+++ stringhelper/helper.go
@@ -1,5 +1,19 @@
 package stringhelper
 
+import (
+	"fmt"
+)
+
+// Upper
 func Upper(text string) string {
 	return text
+}
+
+// Concat
+func Concat(inputs ...string) string {
+	var result string
+	for _, input := range inputs {
+		result = fmt.Sprintf("%s %s", result, input)
+	}
+	return result
 }
